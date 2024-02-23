4c677c607e1268174009b83ed69b8d04976d81e0
Tim <neronovtf@mail.ru>
2024-02-24 01:45:13 +0300

    added cpp file for start box

---
+++ task.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    std::cout << "Hello people !!" << std::endl;
+
+    return 0;
+}
