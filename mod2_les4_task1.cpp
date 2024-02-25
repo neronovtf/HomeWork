400662f467ca5298ac06f76156a033831179c595
Tim <neronovtf@mail.ru>
2024-02-26 02:44:23 +0300

    added files for mod2_les4_task1

---
+++ mod2_les4_task1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Shape {
+protected:
+    int numberCorners;
+    std::string name;
+    Shape(int number, std::string name){
+        this->name = name;
+        this->numberCorners = number;
+        this->print();
+    }
+    void print() {
+        std::cout << "\t" + name + ": " << this->numberCorners << std::endl;
+    }
+public:
+    Shape() {
+        this->name = "Фигура";
+        this->numberCorners = 0;
+        this->print();
+    }
+};
+
+class Triangle : public Shape {
+public:
+    Triangle() :Shape(3, "Треугольник") {};
+};
+
+class Quadrilateral : public Shape {
+public:
+    Quadrilateral() :Shape(4, "Четырёхугольник") {};
+};
+
+int main(){
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Количество сторон:" << std::endl;
+    Shape shape;
+    Triangle triangle;
+    Quadrilateral quadrilateral;
+
+    return 0;
+}
