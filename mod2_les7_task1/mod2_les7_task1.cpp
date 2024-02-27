dc72c93b75a3a934da289188fadfd7ed75c20c5b
Tim <neronovtf@mail.ru>
2024-02-28 00:09:35 +0300

    mod2_les7_tesk1

---
+++ mod2_les7_task1/mod2_les7_task1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int howLength(std::string str, int forbidden_length) {
+
+    int leng = str.length();
+    if (leng != forbidden_length) {
+        return leng;
+    }
+
+    throw "bad_length";
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int lengthWord;
+    std::cout << "Введите запретную длину: ";
+    std::cin >> lengthWord;
+
+    std::string word;
+
+    try{
+        do {
+            std::cout << "Введите слово: ";
+            std::cin >> word;
+
+            std::cout << "Длина слова \"" << word << "\" равна " << howLength(word, lengthWord) << std::endl;
+        }
+        while (true);
+    }
+    catch (const char* error) { std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl; }
+    catch (...) { std::cout << "Неизвестная ошибка" << std::endl; }
+
+    return 0;
+}
