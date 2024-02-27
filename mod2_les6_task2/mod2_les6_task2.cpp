894ed8cd98b3ce46cf075af7f819e7207e8997ad
Tim <neronovtf@mail.ru>
2024-02-27 18:00:38 +0300

    mod2_les6_task2

---
+++ mod2_les6_task2/mod2_les6_task2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define SUB(a, c) ((a) - (c))
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+    return 0;
+}
