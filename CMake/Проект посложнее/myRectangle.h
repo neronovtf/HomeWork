1875639a8ce643bf3639221c75f7a2c2c587eb12
Tim <neronovtf@mail.ru>
2024-03-01 00:38:14 +0300

    CMake. Проект посложнее.

---
+++ CMake/Проект посложнее/myRectangle.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "quadrilateral.h"
+
+class MyRectangle : public Quadrilateral {
+public:
+    MyRectangle();
+};
---
+++ CMake/Проект посложнее/triangleEquilateral.cpp
@@ -0,0 +1,4 @@
+#include "triangle.h"
+#include "triangleEquilateral.h"
+
+TriangleEquilateral::TriangleEquilateral() :Triangle("Равносторонний треугольник", 30, 30, 30, 60, 60, 60) {};
