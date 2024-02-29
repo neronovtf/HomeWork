1c1c99d7ad808b219bf3c46dcd5d5f31b2990d1a
Tim <neronovtf@mail.ru>
2024-03-01 00:36:05 +0300

    CMake. Простейший проект.

---
+++ CMake/Простейший проект/source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string name;
+    std::cout << "Введите имя : ";
+    std::cin >> name;
+
+    std::cout << "Здравствуйте, " + name + "!" << std::endl;
+    return 0;
+}
